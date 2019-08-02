0dc506c4c78dd6b8ab3cf1920bf2fe58e0aed253
johnsoe <evan.johnson.r@gmail.com>
2019-08-02 16:55:52 -0700

    Add lamp mouse follow and lamp prefab

---
+++ Assets/OneNightStand/Scripts/MouseFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+{
+    void Update()
+    {
+        var pos = Camera.main.WorldToScreenPoint(transform.position);
+        var dir = Input.mousePosition - pos;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
