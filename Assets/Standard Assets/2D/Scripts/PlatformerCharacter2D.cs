72262944311ac83cd09a63b0299d03b294a05bb0
Parth <ash.parth.dave@gmail.com>
2019-08-02 17:07:58 -0700

    Add animation for walking

--- Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -26,83 +26,21 @@ namespace UnityStandardAssets._2D
 
         private void FixedUpdate()
         {
-            // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
-            // This can be done using layers instead but Sample Assets will not overwrite your project settings.
-            //Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
-            //for (int i = 0; i < colliders.Length; i++)
-            //{
-            //    if (colliders[i].gameObject != gameObject)
-            //        m_Grounded = true;
-            //}
-            //m_Anim.SetBool("Ground", m_Grounded);
 
-            // Set the vertical animation
-            //m_Anim.SetFloat("vSpeed", m_Rigidbody2D.velocity.y);
         }
 
 
         public void Move(float hMove, float vMove, int focus)
         {
 
-            //// If crouching, check to see if the character can stand up
-            //if (!crouch && m_Anim.GetBool("Crouch"))
-            //{
-            //    // If the character has a ceiling preventing them from standing up, keep them crouching
-            //    if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
-            //    {
-            //        crouch = true;
-            //    }
-            //}
-
-            // Set whether or not the character is crouching in the animator
-            //m_Anim.SetBool("Crouch", crouch);
-
-            //only control the player if grounded or airControl is turned on
-            //if (m_Grounded || m_AirControl)
-            //{
-
-                // The Speed animator parameter is set to the absolute value of the horizontal input.
+            // The Speed animator parameter is set to the absolute value of the horizontal input.
             m_Anim.SetFloat("hSpeed", Mathf.Abs(hMove));
-            m_Anim.SetFloat("vSpeed", Mathf.Abs(hMove));
+            m_Anim.SetFloat("vSpeed", Mathf.Abs(vMove));
 
             // Move the character
             m_Rigidbody2D.velocity = new Vector2(hMove * m_MaxSpeed, vMove * m_MaxSpeed);
 
-            //TODO: Add focus handling
-
-                // If the input is moving the player right and the player is facing left...
-                //if (hMove > 0 && !m_FacingRight)
-                //{
-                //    // ... flip the player.
-                //    Flip();
-                //}
-                //    // Otherwise if the input is moving the player left and the player is facing right...
-                //else if (move < 0 && m_FacingRight)
-                //{
-                //    // ... flip the player.
-                //    Flip();
-                //}
-            //}
-            // If the player should jump...
-            //if (m_Grounded && jump && m_Anim.GetBool("Ground"))
-            //{
-            //    // Add a vertical force to the player.
-            //    m_Grounded = false;
-            //    m_Anim.SetBool("Ground", false);
-            //    m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
-            //}
+            // TODO: Add focus handling
         }
-
-
-        //private void Flip()
-        //{
-        //    // Switch the way the player is labelled as facing.
-        //    m_FacingRight = !m_FacingRight;
-
-        //    // Multiply the player's x local scale by -1.
-        //    Vector3 theScale = transform.localScale;
-        //    theScale.x *= -1;
-        //    transform.localScale = theScale;
-        //}
     }
 }
