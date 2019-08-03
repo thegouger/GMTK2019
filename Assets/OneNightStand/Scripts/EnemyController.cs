478b396e352c255f09fac68bdc58b2dc6e264bd9
johnsoe <evan.johnson.r@gmail.com>
2019-08-03 12:25:32 -0700

    Add Astar pathfinding and base enemy prefab

---
+++ Assets/OneNightStand/Scripts/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+abstract public class EnemyController : MonoBehaviour
+{
+    [SerializeField] internal int health = 1000;
+
+    void Start()
+    {
+    }
+    
+    void Update()
+    {
+    }
+
+    public void Damage(int dmg) {
+        health -= dmg;
+        if (health <= 0) {
+            Die();
+        }
+    }
+
+    // Each individual needs to implement their own death due to animation.
+    internal abstract void Die();
+}
