81b8df56b4bca036aae0353bf47a2291c4fa7685
johnsoe <evan.johnson.r@gmail.com>
2019-08-03 21:08:40 -0700

    Turtle

---
+++ Assets/OneNightStand/Scripts/TurtleMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleMove : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float newDirectionDelay = 3f;
+
+    private Vector2 direction = Vector2.zero;
+    private Rigidbody2D rb2d;
+    private float nextActionTime;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.freezeRotation = true;
+        nextActionTime = Time.time;
+    }
+
+    void Update() {
+        if (Time.time > nextActionTime) {
+            nextActionTime += newDirectionDelay + Random.value;
+            direction = new Vector2(Random.value - 0.5f, Random.value - 0.5f).normalized * speed;
+        }
+    }
+
+    void FixedUpdate() {
+        rb2d.velocity = direction;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        direction = direction * -1;
+    }
+}
