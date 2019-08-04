66fd10bcabcbc2baa6516d68079dd3f94f0ed6e4
Parth <ash.parth.dave@gmail.com>
2019-08-04 05:17:47 -0700

    100% battery to start

--- Assets/OneNightStand/Scripts/GlobalState.cs
+++ Assets/OneNightStand/Scripts/GlobalState.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 public class GlobalState {
 
-    public static float currentBattery = 50f;
+    public static float currentBattery = 100f;
     public static bool[] generators = {false, false, false, false, false};
 
     public void Reset() {
