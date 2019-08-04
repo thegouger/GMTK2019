7396aca513ef0486dae28783782c587e7d59cb05
johnsoe <evan.johnson.r@gmail.com>
2019-08-04 03:47:04 -0700

    Add scenes and some behavior

---
+++ Assets/OneNightStand/Scripts/UIClickHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UIClickHandler : MonoBehaviour
+{
+    public void OnGameStartClick() {
+        Debug.Log("Game start");
+        //TODO: Start first level
+    }
+
+    public void OnControlsClick() {
+        Debug.Log("Controls Clicked");
+        SceneManager.LoadScene("ControlsScreen", LoadSceneMode.Single);
+    }
+
+    public void OnExitClick() {
+        Debug.Log("Exit Clicked");
+        //TODO: Quit
+    }
+
+    public void OnMenuReturnClick() {
+        Debug.Log("Menu Return Clicked");
+        //TODO: Launch main menu
+    }
+}
